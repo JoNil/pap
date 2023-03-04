278000cb72604cf6bea729b5556ee94b4ad1230a
Jonathan Nilsson <jonathan@voysys.se>
2023-03-04 23:41:28 +0100

    Done

--- pap86/src/main.rs
+++ pap86/src/main.rs
@@ -1,6 +1,6 @@
-use std::fs;
-
 use clap::Parser;
+use std::{fmt::Display, fs};
+use strum_macros::{AsRefStr, FromRepr};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -10,7 +10,8 @@ struct Args {
 }
 
 // Register from encoding W | REG
-#[derive(Debug)]
+#[derive(AsRefStr, Copy, Clone, Debug, FromRepr)]
+#[repr(u8)]
 enum Register {
     AL = 0b0000,
     CL = 0b0001,
@@ -30,17 +31,83 @@ enum Register {
     DI = 0b1111,
 }
 
+#[derive(Copy, Clone, Debug, FromRepr)]
+#[repr(u8)]
 enum Opcode {
     Mov = 0b100010,
 }
 
-#[derive(Debug)]
+#[derive(Copy, Clone, Debug)]
 enum Instruction {
-    MovRegReg { from: Register, to: Register },
+    MovRegReg { dst: Register, src: Register },
+}
+
+impl Display for Instruction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Instruction::MovRegReg { dst, src } => {
+                write!(
+                    f,
+                    "mov {}, {}",
+                    dst.as_ref().to_lowercase(),
+                    src.as_ref().to_lowercase()
+                )
+            }
+        }
+    }
 }
 
 fn decode(input: &[u8]) -> Vec<Instruction> {
     let mut res = Vec::new();
+
+    let mut index = 0;
+
+    while index < input.len() {
+        let instruction_byte_1 = input[index];
+        index += 1;
+
+        let opcode = instruction_byte_1 >> 2;
+        let Some(opcode) = Opcode::from_repr(opcode) else {
+            panic!("Invalid Opcode: {opcode:b}")
+        };
+
+        let instruction = match opcode {
+            Opcode::Mov => {
+                let d = (instruction_byte_1 >> 1) & 0b1;
+                let w = instruction_byte_1 & 0b1;
+
+                let instruction_byte_2 = input[index];
+                index += 1;
+
+                let mode = instruction_byte_2 >> 6;
+
+                match mode {
+                    0b11 => {
+                        let w_reg_1 = (w << 3) | ((instruction_byte_2 >> 3) & 0b111);
+                        let w_reg_2 = (w << 3) | (instruction_byte_2 & 0b111);
+
+                        let Some(reg_1) = Register::from_repr(w_reg_1) else {
+                            panic!("Invalid reg: {w_reg_1:b}")
+                        };
+
+                        let Some(reg_2) = Register::from_repr(w_reg_2) else {
+                            panic!("Invalid reg: {w_reg_2:b}")
+                        };
+
+                        let dst = if d == 0b1 { reg_1 } else { reg_2 };
+                        let src = if d == 0b1 { reg_2 } else { reg_1 };
+
+                        Instruction::MovRegReg { dst, src }
+                    }
+                    _ => {
+                        panic!("Unsupported mode!");
+                    }
+                }
+            }
+        };
+
+        res.push(instruction);
+    }
 
     res
 }
@@ -54,5 +121,7 @@ fn main() {
 
     let instructions = decode(&input);
 
-    println!("{instructions:?}")
+    for instruction in &instructions {
+        println!("{instruction}")
+    }
 }
